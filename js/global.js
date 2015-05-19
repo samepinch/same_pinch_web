d721f40e972660c1ce9b0997ceffa18f5b9ba1fc
imaginationcoder <labs@maisasolutions.com>
2015-05-19 18:29:53 +0530

    ajax call for post display on home page

--- js/global.js
+++ js/global.js
@@ -1,12 +1,4 @@
-$(function(){
-    $('#news-container').vTicker({
-        speed: 1000,
-        pause: 4000,
-        animation: 'fade',
-        mousePause: false,
-        showItems: 3
-    });
-});
+
 
 $('.carousel').carousel({
     interval: 3000,
@@ -21,7 +13,24 @@ $( document ).ready(function() {
         url : "https://msocl.herokuapp.com/api/posts.json",
         data:{command: "web_view"},
         success: function(data){
-            //alert(JSON.stringify(data));
+            $('.dynamic_scrol').html("");
+            $.each(data.body.posts, function( index, value ) {
+                $(".dynamic_scrol").append('<li><div class="post-wrap"><div class="post-box">'+
+                '<div class="col-md-2"><div class="profilepic-position"><img src="'+ value.photo +'" width="43" height="43" class="profile-pic" alt=""/></div></div>'+
+                '<div class="col-md-10"><h4 style="display:inline-block">'+ value.name +'</h4> <span class="pull-right txt-light">'+ value.createdAt +'</span></div>'+
+                '<div class="clearfix"></div>'+
+                '<div class="col-md-10 col-md-offset-2"> '+ value.content +' </div>'+
+                '</div></div></li>')
+            });
+            $(function(){
+                $('#news-container').vTicker({
+                    speed: 1000,
+                    pause: 4000,
+                    animation: 'fade',
+                    mousePause: false,
+                    showItems: 3
+                });
+            });
         }
     });
 });
